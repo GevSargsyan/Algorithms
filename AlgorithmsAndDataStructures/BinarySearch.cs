8a357e4f3c4d917b00f62bdf08f56c46a8e96d9a
GevSargsyan <gevorgsargsyan8@gmail.com>
2024-01-07 22:09:56 +0400

    Added Binary Search Algorithm

---
+++ AlgorithmsAndDataStructures/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace AlgorithmsAndDataStructures
+{
+    public static class BinarySearch
+    {
+        public static int BinarySearchMethod(List<int> array, int search)//0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 
+        {
+            int left = 0;
+            int right = array.Count - 1;
+            int middle;
+
+            while (left <= right)
+            {
+                middle = (left + right) / 2;
+                if (search < array[middle])
+                {
+                    right = middle - 1;
+                }
+                else if (search > array[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -1,4 +1,4 @@
-﻿using AlgorithmsAndDataStructures.Extensions;
+﻿using System.Diagnostics;
 
 namespace AlgorithmsAndDataStructures
 {
@@ -6,11 +6,21 @@ namespace AlgorithmsAndDataStructures
     {
         static void Main(string[] args)
         {
-            long[] phoneNumbers = new long[] { +374123456L, +374001234L, +374123456L, +374374374L, +374001234L };
-            Console.WriteLine(Arrays.DistinctArray(phoneNumbers).ToArrayString());
+            //BinarySearch.BinarySearchMethod(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 }, 15);
 
-            int[] numbers = new int[] { 1, 2, 2, 3, 4, 4, 5 };
-            Console.WriteLine(Arrays.DistinctSorted(numbers).ToArrayString());
+            List<int> list = new List<int>();
+            for (int i = 0; i < 1000000000; i++)
+            {
+                list.Add(i);
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int index = list.IndexOf(100000000);//00:00:00.0401065
+            BinarySearch.BinarySearchMethod(list, 100000000);// 00:00:00.0003057    (LOG2^N);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+
         }
     }
 }
